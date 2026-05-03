e36f17d85ea05c44f554666f74f5a4f5b0c26809
b12346443-droid <b12346443@gmail.com>
2026-05-03 12:47:28 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+client.once("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("guildMemberAdd", member => {
+    const channel = member.guild.channels.cache.find(
+        ch => ch.name === "🙋‍♂️丨الــتــرحــيــب・𝑤𝑒𝑙𝑐𝑜𝑚𝑒"
+    );
+
+    if (!channel) return;
+
+    const memberCount = member.guild.memberCount;
+
+    channel.send(
+`♥︎ Welcome ${member}
+★ You are member number ${memberCount}`
+    );
+});
+
+client.login("MTUwMDIxNzI3MTE1NzUyNjcxOA.GcVqkF.oLPn9nv-nW-GXFt8Newvunek1ooaS8eVdxCHFM");
